fb00dba82ae51f5d537f85f00f2eeeac947ca20f
gaoyabo <gaoyabo_test@163.com>
2018-05-31 17:43:39 +0800

    引入模块错误，已更改为django.urls

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
+from lists.views import home_page
+from django.urls import resolve
 
 # Create your tests here.
-class SomkeTest(TestCase):
+class HomePageTest(TestCase):
     
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, homepage)
+
